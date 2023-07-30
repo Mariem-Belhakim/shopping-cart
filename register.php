f092e8e0d798c92efaa3dbf5e29e6a8ea3c86add
Mariem Belhakim <contact.belhakim@gmail.com>
2023-07-30 14:02:55 +0100

    style

--- register.php
+++ register.php
@@ -36,16 +36,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Register</title>
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
     <style>
         .bg{
             background: #1690A7;
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 100vh;
+            /* height: 100vh; */
         }
         form{
             width:500px ;
@@ -63,7 +62,7 @@
             border: 2px solid #ccc;
             width: 95%;
             padding: 10px;
-            margin: 10px auto;
+            margin: 3px auto;
             border-radius: 5px;
         }
         label{
@@ -88,8 +87,8 @@
 <body>
 <?php include("nav.php"); ?>
   <div class="bg">
- <form action="" method="POST">
-    <h2>Login</h2>
+ <form action="" class="my-4" method="POST">
+    <h2>Register</h2>
 
     <?php
                 
